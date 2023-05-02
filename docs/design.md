3d578bb00f1c44fdd12f0e56ecb94f6eab77faf8
kang-sw <ki6080@gmail.com>
2023-05-03 06:14:20 +0900

    [wip] System modelling

--- docs/design.md
+++ docs/design.md
@@ -17,25 +17,26 @@ erDiagram
     TranslatedLine {
         int translation_id PK "생성 순서로 부여"
         int line_number FK
-        int token_consumed "이 라인에 사용된 토큰 개수"
+        int ntok_prompt "번역에 사용된 토큰 개수"
+        int ntok_reply "응답에 사용된 토큰 개수"
         string content "번역된 문자열"
     }
-
-    Book ||--o{ Line: "has"
-
-    Library ||--o{ Book: "has"
-
-    Book ||--|| ReaderContext: "has"
-
-    Book ||--o{ DictArg: "has"
 
     ReaderContext {
         string source_lang "원문 언어"
         string alias "책 별명"
     }
 
-    Line ||--o{ TranslatedLine: candidates
+    Library ||--o{ Book: "has"
+    Book ||--|| BookDB: "has"
+    Book ||--|| ReaderContext: "has"
+    BookDB ||--o{ Line: "has"
+    BookDB ||--o{ DictArg: "has"
+    Line ||--o{ TranslatedLine: is
 ```
 
 - `Line::state`
-  - `0 :=` 암묵적 승인.
+  - `0 :=` 암묵적 승인. 앞에서
+  - `1 :=` 명시적 승인. 어떤 경우에도 추가적인 번역은 일어나지 않습니다.
+  - `2 :=` 개별 재번역. 이 문장만 다시 번역될 예정입니다.
+  - `3 :=` 이후 전체 재번역. 이 행 이후의 모든 라인이 invalidation 됩니다.
--- lib/src/book.rs
+++ lib/src/book.rs
@@ -18,6 +18,28 @@ pub struct BookConfig {
     alias: String,
 }
 
+pub struct Line {
+    pub line_number: usize,
+    pub source: String,
+    pub translation_id: Option<usize>,
+    pub state: LineState,
+}
+
+pub struct TranslatedLine {
+    pub id: u64,
+    pub line_number: usize,
+    pub ntok_prompt: usize,
+    pub ntok_reply: usize,
+    pub content: String,
+}
+
+pub enum LineState {
+    ImplicitConfirm = 0,
+    ExplicitConfirm = 1,
+    Invalidated = 2,
+    InvalidateAllAfter = 3,
+}
+
 /* --------------------------------------- Creation Logics -------------------------------------- */
 #[derive(Debug, thiserror::Error)]
 pub enum BookCreationError {}
@@ -44,6 +66,11 @@ impl Book {
         book_name: &str,
         content: &mut dyn Iterator<Item = &str>,
     ) -> Result<Book, BookCreationError> {
+        // 1. 폴더 생성 -> book_name은 base64로 컨버전 후 기록
+        // 2. 텍스트 모두 임포트 -> 행 트리밍, 빈 행 삭제 등 작업 수행
+        // 3. 책 관련 파일 생성: DB, Config, ...
+        // 4. book.info 파일 생성
+
         todo!()
     }
 }
@@ -55,6 +82,13 @@ pub enum BookOpenError {}
 impl Book {
     /// 이미 존재하는 책을 엽니다.
     pub fn open(library_dir: &Path, book_name: &str) -> Result<Book, BookOpenError> {
+        // 1.
+
+        todo!()
+    }
+
+    /// 폴더 내 모든 책 이름을 열거합니다.
+    pub fn list(library_dir: &Path) -> std::io::Result<Vec<String>> {
         todo!()
     }
 }
