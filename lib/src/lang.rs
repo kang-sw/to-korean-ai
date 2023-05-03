21a3f80f199e853506de7afd3d291d355072ec2b
kang-sw <ki6080@gmail.com>
2023-05-03 18:05:36 +0900

    lib: Implement basic translation routine

--- lib/src/lang.rs
+++ lib/src/lang.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 
 /// 각종 시스템 프롬프트를 생성하기 위한 프로필입니다.
 pub trait PromptProfile: Send + Sync + std::fmt::Debug {
-    fn proper_noun_instruction(&self, src_lang: Language) -> &str;
+    fn proper_noun_instruction(&self, src_lang: Language) -> Cow<str>;
 
     /// 서버 응답을 파싱해서 고유명사 리스트 추출. Parse 실패 시 None
     fn parse_proper_noun_output(
@@ -11,12 +11,6 @@ pub trait PromptProfile: Send + Sync + std::fmt::Debug {
         src_lang: Language,
         response: &str,
     ) -> Option<Vec<CompactString>>;
-
-    /// 입력으로부터 등장 인물 사이의 관계를 파악하고, 인물 사이의 대화에서 분위기 등을 조정하기
-    /// 위해 활용합니다.
-    ///
-    /// - `previous_note`: 앞서 정리된 출력입니다. 관계도는 이를 바탕으로 업데이트됩니다.
-    fn query_relationship(&self, src_lang: Language, prev_note: Option<&str>) -> Cow<str>;
 
     /// 번역 단계에서 제공: 적절한 번역 지시 사항을 지정합니다. 핵심이 되는 프롬프트입니다.
     fn trans_instruction(&self, src_lang: Language) -> Cow<str>;
@@ -27,6 +21,10 @@ pub trait PromptProfile: Send + Sync + std::fmt::Debug {
         dest_lang: Language,
         dict: &[(&str, &str)],
     ) -> Cow<str>;
+
+    /// 번역 단계에서 제공: 제공된 원문의 앞부분을 제공합니다.
+    fn trans_appendix_leading_context_content(&self, src_lang: Language, content: &str)
+        -> Cow<str>;
 
     /// 번역 단계에서 제공: 인물 사이의 관계 서술에 대한 시스템 안내 프롬프트입니다.
     fn trans_appendix_relationship(&self, src_lang: Language, desc: &str) -> Cow<str>;
@@ -85,15 +83,15 @@ pub mod profiles {
                 };
 
                 match $source_lang {
-                    Language::English => &EN,
-                    Language::Japanese => &JP,
+                    Language::English => EN.as_str(),
+                    Language::Japanese => JP.as_str(),
                     Language::Korean => panic!("Korean -> Korean not allowed!"),
                 }
             }};
         }
 
         impl super::super::PromptProfile for KoreanV1 {
-            fn proper_noun_instruction(&self, source_lang: Language) -> &str {
+            fn proper_noun_instruction(&self, source_lang: Language) -> Cow<str> {
                 static_lang_str_ko!(
                     r##"    제시된 {{LANG}} 원문으로부터,
                  
@@ -102,7 +100,7 @@ pub mod profiles {
                     
                             출력 형식은 YAML입니다."##,
                     source_lang
-                )
+                ).into()
             }
 
             fn parse_proper_noun_output(
@@ -115,7 +113,8 @@ pub mod profiles {
                 // 마크다운 문법으로 YAML 받음 ... 먼저 앞뒤의 ```yaml ~~ ``` 떼어낸다.
                 let lines: Vec<_> = response
                     .lines()
-                    .skip_while(|line| !line.starts_with("```yaml"))
+                    .skip_while(|line| line.starts_with("```yaml") == false)
+                    .skip(1)
                     .collect();
 
                 let close_pos = lines
@@ -150,56 +149,18 @@ pub mod profiles {
 
             fn trans_instruction(&self, source_lang: Language) -> Cow<str> {
                 match source_lang {
-                Language::English => todo!(),
-                Language::Korean => panic!("Korean -> Korean not allowed!"),
-                Language::Japanese => indoc!(
-                    r##"    # 지시사항
-                    
-                            입력되는 일본어 문장을 한국어로 번역한다. 다음은 그 요구 조건이다.
-        
-                            * 제공되는 모든 문장은 연결된 내용이므로, 입력된 원문의 맥락을 적극적으로 고려한다.
-                            * (가능하다면) 일본어 관용구는 적절하게 대응되는 한국어 관용구로 가공한다.
-                            * 올바른 한국어 문장으로 가공한다.
-                            * 등장인물의 대사를 포함한 모든 문장에서, 존댓말의 사용을 최소화한다."##
-                ),
-            }.into()
-            }
+                    Language::English => todo!(),
+                    Language::Korean => panic!("Korean -> Korean not allowed!"),
+                    Language::Japanese => indoc!(
+                        r##"    # 기본 지시사항
 
-            fn query_relationship(
-                &self,
-                source_lang: Language,
-                previous_note: Option<&str>,
-            ) -> Cow<str> {
-                // TODO: previous_note 유/무에 따라 다른 프롬프트 제공. previous_note 있으면
-                // 참고하여 관계도를 업데이트하도록 유도
+                                제공되는 일본어 소설을 한국어로 번역해야 한다. 다음은 그 요구 조건이다.
 
-                /*
-
-                **지시사항**
-
-                - 제공된 일본어 원문으로부터, 등장인물 사이의 관계를 그래프 형태로 작성한다.
-                - 중요도가 낮아 보이는 인물은 무시한다.
-
-                (도중에 맥락에 무관하게 장면이 변경될 수 있음)
-
-                **출력 형식**
-
-                - 등장인물 -> 다른등장인물: 상대에 대한 감정 및 사회적인 관계
-
-                (예시)
-                A -> B: 대등한 친구이며, 친애한다.
-                B -> A: 대등한 친구이지만, 죄의식을 갖고 있다.
-                C -> A: 무관한 사람이지만, 적대시한다.
-
-                ---
-
-                각 관계가 중복되지 않도록 주의하십시오.
-
-                지금부터 일본어 원문을 입력할 것입니다. 준비되었습니까?
-
-                */
-
-                default()
+                                * 등장인물의 대사를 포함한 모든 문장에서, 존댓말의 사용을 최소화한다.
+                                * 원문의 뉘앙스를 유지하되, 자연스러운 한국어 문장으로 가공한다."##
+                    ),
+                }
+                .into()
             }
 
             fn trans_appendix_proper_noun_dict(
@@ -216,11 +177,23 @@ pub mod profiles {
                 #[allow(unused_must_use)]
                 {
                     let b: &mut dyn std::fmt::Write = &mut buf;
-                    writeln!(b, "다음은 고유 명사의 번역을 제안하는 사전입니다:");
-                    writeln!(b, "");
-                    writeln!(b, "[시작]");
+                    writeln!(
+                        b,
+                        indoc!(
+                            r##"    ## 부록: 사전
+                                    
+                                    다음은 고유 명사의 번역에 참고할 수 있는 사전이다.
+                                    
+                                    [시작]"##
+                        )
+                    );
 
                     for (src, dest) in dict {
+                        if dest.is_empty() {
+                            log::warn!("empty translation for {} included", src);
+                            continue;
+                        }
+
                         writeln!(b, "* {} -> {}", src, dest);
                     }
 
@@ -237,6 +210,14 @@ pub mod profiles {
 
                 // TODO: 다음은 인물 사이의 관계를 정리한 노트입니다. 참고하여 번역하십시오
 
+                default()
+            }
+
+            fn trans_appendix_leading_context_content(
+                &self,
+                src_lang: Language,
+                content: &str,
+            ) -> Cow<str> {
                 default()
             }
         }
--- lib/src/translate.rs
+++ lib/src/translate.rs
@@ -1,10 +1,13 @@
 //! 번역 작업을 위한 OpenAI API 프롬프트 추상화
 
-use std::sync::Arc;
+use std::{borrow::Cow, sync::Arc};
 
 use async_openai::{
     error::OpenAIError,
-    types::{ChatCompletionRequestMessage, CreateChatCompletionRequest, Role},
+    types::{
+        ChatChoice, ChatCompletionRequestMessage, CreateChatCompletionRequest,
+        CreateChatCompletionResponse, Role,
+    },
 };
 use compact_str::CompactString;
 use default::default;
@@ -19,26 +22,88 @@ pub struct Instance {
     ai: async_openai::Client,
 }
 
-#[derive(Debug, derive_more::Display)]
+#[allow(non_camel_case_types)]
+#[derive(Debug, derive_more::Display, Default, Clone, Copy)]
 pub enum ChatModel {
     #[display(fmt = "gpt-3.5-turbo")]
-    Gpt3_5,
+    #[default]
+    Gpt_3_5_Turbo,
+
+    #[display(fmt = "gpt-4")]
+    Gpt_4,
 }
 
 /// 번역에 필요한 정적 세팅(모델 정보, 토큰 길이 등)을 포함합니다.
-#[derive(Debug, typed_builder::TypedBuilder)]
+#[derive(Debug, typed_builder::TypedBuilder, Clone)]
 pub struct Settings {
     source_lang: Language,
     profile: Arc<dyn PromptProfile>,
+
+    #[builder(default)]
     model: ChatModel,
+
+    #[builder(default, setter(strip_option))]
+    temperature: Option<f32>,
 }
 
-/* ------------------------------------------ Init Ops ------------------------------------------ */
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("OpenAI API error: {0}")]
+    OpenAI(#[from] OpenAIError),
+
+    #[error("No choice found")]
+    NoChoice,
+
+    #[error("Failed to parse output noun list during ProperNoun generation")]
+    NounListParseFailed,
+
+    #[error("Token length limit exceeded.")]
+    TokenLengthLimit,
+
+    #[error("Line count mismatch: expected {expected}, actual {actual}")]
+    LineCountMismatch { expected: usize, actual: usize },
+}
+
+/* ------------------------------------------ Core Ops ------------------------------------------ */
+struct CallResult {
+    assistant_reply: String,
+    num_prompt_tokens: usize,
+    num_compl_tokens: usize,
+}
+
 impl Instance {
     pub fn new(api_key: &str) -> Self {
         Self {
             ai: async_openai::Client::new().with_api_key(api_key),
         }
+    }
+
+    async fn _call(&self, req: CreateChatCompletionRequest) -> Result<CallResult, Error> {
+        let rep = self.ai.chat().create(req).await?;
+        let msg = rep.choices.into_iter().next().ok_or(Error::NoChoice)?;
+
+        if let Some(true) = msg.finish_reason.as_ref().map(|x| x == "length") {
+            return Err(Error::TokenLengthLimit);
+        }
+
+        let (n_prompt, n_compl) = rep
+            .usage
+            .map(|x| (x.prompt_tokens, x.completion_tokens))
+            .ok_or(Error::NoChoice)?;
+
+        Ok(CallResult {
+            assistant_reply: msg.message.content,
+            num_prompt_tokens: n_prompt as _,
+            num_compl_tokens: n_compl as _,
+        })
+    }
+}
+
+fn _gen<'a>(role: Role, msg: impl Into<Cow<'a, str>>) -> ChatCompletionRequestMessage {
+    ChatCompletionRequestMessage {
+        role,
+        content: msg.into().into(),
+        ..default()
     }
 }
 
@@ -54,75 +119,156 @@ pub struct ProperNounRetrievalResult {
     _no_build: (),
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum ProperNounRetrievalError {
-    #[error("OpenAI API error: {0}")]
-    OpenAI(#[from] OpenAIError),
-
-    #[error("No choice found")]
-    NoChoice,
-
-    #[error("The translation profile failed to parse output message")]
-    NounListParseFailed,
-
-    #[error("Token length limit exceeded.")]
-    TokenLengthLimit,
-}
-
 impl Instance {
     pub async fn retrieve_proper_nouns(
         &self,
         content: &str,
         opt: &Settings,
-    ) -> Result<ProperNounRetrievalResult, ProperNounRetrievalError> {
-        let mut req = CreateChatCompletionRequest::default();
-        req.model = opt.model.to_string();
+    ) -> Result<ProperNounRetrievalResult, Error> {
+        let mut req = opt.new_chat_req();
+        let lang = opt.source_lang;
 
-        // 프롬프트를 빌드한다
-        let msg = |role: Role, msg: &str| ChatCompletionRequestMessage {
-            role,
-            content: msg.into(),
-            ..default()
-        };
-
-        req.messages.push(msg(
+        req.messages.push(_gen(
             Role::System,
-            opt.profile.proper_noun_instruction(opt.source_lang),
+            opt.profile.proper_noun_instruction(lang),
         ));
 
-        req.messages.push(msg(Role::User, content));
-        let rep = self.ai.chat().create(req).await?;
-        let msg = rep
-            .choices
-            .first()
-            .ok_or(ProperNounRetrievalError::NoChoice)?;
-
-        if let Some(true) = msg.finish_reason.as_ref().map(|x| x == "length") {
-            return Err(ProperNounRetrievalError::TokenLengthLimit);
-        }
+        req.messages.push(_gen(Role::User, content));
+        let rep = self._call(req).await?;
 
         let nouns = opt
             .profile
-            .parse_proper_noun_output(opt.source_lang, &msg.message.content)
-            .ok_or(ProperNounRetrievalError::NounListParseFailed)?;
-
-        let (n_prompt, n_compl) = rep
-            .usage
-            .map(|x| (x.prompt_tokens, x.completion_tokens))
-            .ok_or(ProperNounRetrievalError::NoChoice)?;
+            .parse_proper_noun_output(lang, &rep.assistant_reply)
+            .ok_or(Error::NounListParseFailed)?;
 
         Ok(ProperNounRetrievalResult {
             nouns,
-            num_prompt_tokens: n_prompt as _,
-            num_compl_tokens: n_compl as _,
+            num_prompt_tokens: rep.num_prompt_tokens,
+            num_compl_tokens: rep.num_compl_tokens,
             _no_build: (),
         })
     }
 }
 
+impl Settings {
+    fn new_chat_req(&self) -> CreateChatCompletionRequest {
+        let mut req = CreateChatCompletionRequest::default();
+        req.model = self.model.to_string();
+        req.temperature = self.temperature;
+        req.messages.reserve(10);
+        req
+    }
+}
+
 /* --------------------------------------- Translation Ops -------------------------------------- */
 
-impl Instance {}
+/// 번역기에 제공할 입력 컨텍스트
+///
+/// TODO: 등장 인물 관계 정보?
+#[derive(typed_builder::TypedBuilder, Debug)]
+pub struct TranslationInputContext<'a> {
+    /// 단순히 앞부분의 원문입니다. 적당한 양을 잘라서 넣으면, 앞부분의 맥락을 파악하기 위한
+    /// 프롬프트를 추가적으로 생성합니다.
+    leading_content: Option<&'a str>,
+
+    /// 고유 명사의 Dictionary 정보입니다.
+    dictionary: &'a [(&'a str, &'a str)],
+}
+
+/// 번역 결과를 담습니다.
+pub struct TranslationResult {
+    /// 번역된 문장의 인덱스 및 내용을 담습니다.
+    source_string: String,
+
+    /// (Position, Size)
+    lines: Vec<(usize, usize)>,
+
+    /// 번역에 사용한 토큰 개수입니다.
+    pub num_prompt_tokens: usize,
+    pub num_compl_tokens: usize,
+
+    _no_build: (),
+}
+
+impl Instance {
+    /// 제공된 문장에 대해 한 번의 번역 작업을 수행합니다.
+    ///
+    /// - `content`: 번역할 문장의 리스트입니다.
+    ///
+    pub async fn translate(
+        &self,
+        input_ctx: &TranslationInputContext<'_>,
+        content: &mut dyn Iterator<Item = &str>,
+        opt: &Settings,
+    ) -> Result<TranslationResult, Error> {
+        let mut req = opt.new_chat_req();
+        let lang = opt.source_lang;
+
+        // 번역기 지시사항 전달
+        req.messages
+            .push(_gen(Role::System, opt.profile.trans_instruction(lang)));
+
+        // 부록에 고유 명사 사전 정의
+        {
+            let dict_prompt = opt
+                .profile
+                .trans_appendix_proper_noun_dict(lang, input_ctx.dictionary);
+
+            if dict_prompt.is_empty() == false {
+                req.messages.push(_gen(Role::System, dict_prompt));
+            }
+        }
+
+        // 부록에 맥락 파악을 위한 원문 추가
+        if let Some(x) = input_ctx
+            .leading_content
+            .map(|x| opt.profile.trans_appendix_leading_context_content(lang, x))
+            .filter(|x| x.is_empty() == false)
+        {
+            req.messages.push(_gen(Role::System, x));
+        }
+
+        // 번역할 문장 작성
+        let (src_line_count, content) = content.map(|x| x.trim()).fold(
+            (0, String::with_capacity(1024)),
+            |(mut c, mut base), x| {
+                c += 1;
+
+                base.push_str(x);
+                base.push_str("\n\n");
+                (c, base)
+            },
+        );
+
+        req.messages.push(_gen(Role::User, content));
+
+        // 번역 요청
+        let rep = self._call(req).await?;
+        let base = rep.assistant_reply.as_bytes().as_ptr();
+
+        let result = TranslationResult {
+            lines: rep
+                .assistant_reply
+                .lines()
+                .filter_map(|x| Some(x.trim()).filter(|x| x.is_empty() == false))
+                .map(|x| (x.as_bytes().as_ptr() as usize - base as usize, x.len()))
+                .collect(),
+            source_string: rep.assistant_reply,
+            num_prompt_tokens: rep.num_prompt_tokens,
+            num_compl_tokens: rep.num_compl_tokens,
+            _no_build: default(),
+        };
+
+        if result.lines.len() != src_line_count {
+            return Err(Error::LineCountMismatch {
+                expected: src_line_count,
+                actual: result.lines.len(),
+            });
+        }
+
+        Ok(result)
+    }
+}
 
 /* ---------------------------------------------------------------------------------------------- */
 /*                                          TEST SECTION                                          */
@@ -186,7 +332,7 @@ mod __test {
             let setting = Settings::builder()
                 .source_lang(Language::Japanese)
                 .profile(Arc::new(lang::profiles::KoreanV1))
-                .model(super::ChatModel::Gpt3_5)
+                .model(super::ChatModel::Gpt_3_5_Turbo)
                 .build();
 
             let res = h.retrieve_proper_nouns(&content, &setting).await;
